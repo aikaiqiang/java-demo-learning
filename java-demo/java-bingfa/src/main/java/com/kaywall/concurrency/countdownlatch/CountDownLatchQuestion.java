d23bdf859e41653014116bd51b58a713b860944e
aikaiqiang <>
2019-07-05 00:10:05 +0800

    :rocket: synchronized 可重入锁

--- java-demo/java-bingfa/src/main/java/com/kaywall/concurrency/countdownlatch/CountDownLatchQuestion.java
+++ java-demo/java-bingfa/src/main/java/com/kaywall/concurrency/countdownlatch/CountDownLatchQuestion.java
@@ -1,7 +1,9 @@
 package com.kaywall.concurrency.countdownlatch;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -17,20 +19,27 @@ public class CountDownLatchQuestion {
 //		CountDownLatch countDownLatch = new CountDownLatch(5);
 
 		// jdk 1.4 实现 CountDownLatch
-//		CustomCountDownLatch countDownLatch = new CustomCountDownLatch(5);
+		CustomCountDownLatch countDownLatch = new CustomCountDownLatch(5);
 
 		// jdk 1.5 实现 CountDownLatch
-		CustomCountDownLatch2 countDownLatch = new CustomCountDownLatch2(5);
+//		CustomCountDownLatch2 countDownLatch = new CustomCountDownLatch2(5);
 
 		for (int i = 0; i < 5; i++) {
-			threadPool.submit(() -> {
+			threadPool.execute(() -> {
 				action();
+				try {
+					// 每个线程睡眠 10s
+					Thread.sleep(10 * 1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
 				countDownLatch.countDown();
 			});
 		}
 
 		countDownLatch.await();
-
+		notifyWaitThread();
 		System.out.println("to end");
 		// 关闭线程池
 		threadPool.shutdown();
@@ -49,16 +58,21 @@ public class CountDownLatchQuestion {
 
 		public void countDown() {
 			synchronized (this) {
+				printLockObject(this);
 				if(count < 1){
 					return;
 				}
 				count--;
 				// 当 count == 0 唤起阻塞线程
 				if(count == 0){
-					notifyAll();
+//					notifyAll();
+					notify();
+					notifyThread();
 				}
+				printReleaseLockObject(this);
 			}
 		}
+
 
 		public void await() throws InterruptedException {
 			// 判断阻塞状态并清除
@@ -66,9 +80,18 @@ public class CountDownLatchQuestion {
 				throw  new InterruptedException();
 			}
 			synchronized (this) {
+				printLockObject(this);
 				while (count > 0){
+					currentWaitThread();
+					// 阻塞等待被唤醒
+					long waitStart = System.currentTimeMillis();
+					// 此时调用 wait() 时， 进入 wait() 方法后，当前线程释放锁, 在从 wait() 返回前，线程与其他线程竞争重新获得锁
+					// 只能在同步方法或同步块中调用 wait()方法
 					wait();
+					// 当被其他线程 notify() / notifyAll() 继续执行下面步骤
+					System.out.println("weak up, wait cost = " + (System.currentTimeMillis() - waitStart) + "ms");
 				}
+				printReleaseLockObject(this);
 			}
 		}
 	}
@@ -93,6 +116,7 @@ public class CountDownLatchQuestion {
 				// 当 count == 0 唤起阻塞线程
 				if(count == 0){
 					condition.signal();
+					notifyThread();
 				}
 			} finally {
 				lock.unlock();
@@ -108,6 +132,7 @@ public class CountDownLatchQuestion {
 			lock.lock();
 			try {
 				while (count > 0){
+					currentWaitThread();
 					condition.await();
 				}
 			} finally {
@@ -118,6 +143,28 @@ public class CountDownLatchQuestion {
 
 	private static void action(){
 		System.out.printf("[线程：%s] 正在执行。。。\n", Thread.currentThread().getName());
+	}
+
+
+	private static void currentWaitThread(){
+		System.out.printf("[线程：%s] 阻塞等待唤醒。。。\n", Thread.currentThread().getName());
+	}
+
+
+	private static void notifyWaitThread(){
+		System.out.printf("[线程：%s] 被唤醒 -_-\n", Thread.currentThread().getName());
+	}
+
+	private static void notifyThread(){
+		System.out.printf("[线程：%s] 调用notify，唤醒等待线程\n", Thread.currentThread().getName());
+	}
+
+	private static void printLockObject(Object lock){
+		System.out.printf("[线程：%s] 锁住对象={%s}\n", Thread.currentThread().getName(), lock);
+	}
+
+	private static void printReleaseLockObject(Object lock){
+		System.out.printf("[线程：%s] 释放锁对象={%s}\n", Thread.currentThread().getName(), lock);
 	}
 
 }
