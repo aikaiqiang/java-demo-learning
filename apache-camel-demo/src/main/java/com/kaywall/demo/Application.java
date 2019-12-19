d0602cb90054c0c43db56400b71b8390b87b6f15
aikaiqiang <akqdream@gmail.com>
2019-12-19 18:22:33 +0800

    :rocket: apache-camel example

---
+++ apache-camel-demo/src/main/java/com/kaywall/demo/Application.java
@@ -0,0 +1,68 @@
+package com.kaywall.demo;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.main.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *  Camel examples： https://github.com/camelinaction/camelinaction
+ * @author aikaiqiang
+ * @date 2019年12月19日 17:37
+ */
+public class Application {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) throws Exception {
+		Main main = new Main();
+		main.addRouteBuilder(new MyRouteBuilder());
+		main.run(args);
+	}
+
+	private static class MyRouteBuilder extends RouteBuilder {
+		@Override
+		public void configure() throws Exception {
+			from("timer:simple?period=503")
+					.id("simple-route")
+					.transform()
+					.exchange(this::dateToTime)
+					.process()
+					.message(this::log)
+					.process()
+					.body(this::log)
+					.choice()
+					.when()
+					.body(Integer.class, b -> (b & 1) == 0)
+					.log("Received even number")
+					.when()
+					.body(Integer.class, (b, h) -> h.containsKey("skip") ? false : (b & 1) == 0)
+					.log("Received odd number")
+					.when()
+					.body(Objects::isNull)
+					.log("Received null body")
+					.when()
+					.body(Integer.class, b -> (b & 1) != 0)
+					.log("Received odd number")
+					.endChoice();
+		}
+
+		private Long dateToTime(Exchange e) {
+			return e.getProperty(Exchange.TIMER_FIRED_TIME, Date.class).getTime();
+		}
+
+		private void log(Object b) {
+			LOGGER.info("body is: {}", b);
+		}
+
+		private void log(Message m) {
+			LOGGER.info("message is: {}", m);
+		}
+
+	}
+}
---
+++ apache-camel-demo/src/test/java/com/kaywall/demo/Test.java
@@ -0,0 +1,9 @@
+package com.kaywall.demo;
+
+/**
+ *  E
+ * @author aikaiqiang
+ * @date 2019年12月19日 17:37
+ */
+public class Test {
+}
